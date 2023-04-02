03e926a5c971480f8c33a1679825ca2254254eea
Shreya Deb <97588880+Shreyax29@users.noreply.github.com>
2023-04-02 16:15:02 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "The brain of any computer system is?",
+    answer: "CPU",
+    options: [
+      "ALU",
+      "Memory",
+      "CPU",
+      "Control Unit"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The binary system uses powers of",
+    answer: "2",
+    options: [
+      "2",
+      "10",
+      "8",
+      "16"
+    ]
+  },
+    {
+    numb: 3,
+    question: "A single packet on a data link is known as?",
+    answer: "Frame",
+    options: [
+      "Path",
+      "Frame",
+      "Segment",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "ASCII stands for?",
+    answer: "American standard code for information interchange",
+    options: [
+      "American standard code for information interchange",
+      "All purpose scientific code for information interchange",
+      "American security code for information interchange",
+      "None of the above"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
